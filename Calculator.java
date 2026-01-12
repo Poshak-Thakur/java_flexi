dce50a06905ed27c7dd910a8df78393989b2de28
Poshak Kumar Thakur <thakurposhak@gmail.com>
2026-01-12 10:55:49 +0545

    Addition using calculator

---
+++ Calculator.java
@@ -0,0 +1,13 @@
+public class Calculator {
+    public int add (int a, int b){return a + b;}
+    public int subtract (int a, int b){return a-b;}
+    public int multiply(int a, int b){return a*b;}
+    public int divide(int a, int b){
+        if(b ==0){System.out.println("Error: Division by zero"); return 0;}
+        else{return a/b;}
+    }
+    public static void main(String[] args){
+        Calculator calc = new Calculator();
+        System.out.println("Addition: " + calc.add(10,15));
+    }
+}
