2b5af047437bf0d2bb2e6d363bd5b1e8ce72c09b
Poshak Kumar Thakur <thakurposhak@gmail.com>
2026-01-18 10:45:01 +0545

    java code to print name,prn and address in different lines

---
+++ Intro.java
@@ -0,0 +1,27 @@
+//java program to print name, prn and address in three different lines
+class Intro {
+    public static void main(String[] args){
+        System.out.println("Name: Poshak Thakur");
+        System.out.println("PRN: 24070111236");
+        System.out.println("Address: Pune, Maharashtra");
+    }
+}
+
+//taking user input for name, prn and address and printing them in three different lines
+// import java.util.Scanner;    
+// public class Intro {
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Enter your name: ");
+//         String name = sc.nextLine();
+//         System.out.print("Enter your PRN: ");
+//         String prn = sc.nextLine();
+//         System.out.print("Enter your address: ");
+//         String address = sc.nextLine();
+//         System.out.println("Name: " + name);
+//         System.out.println("PRN: " + prn);
+//         System.out.println("Address: " + address);
+//     }
+// }
+
+
