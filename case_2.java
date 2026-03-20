a0a6e8290f6ba1b0fa245d95df0fb0da9de5c8d5
Poshak Kumar Thakur <thakurposhak@gmail.com>
2026-03-20 15:45:39 +0545

    case_2

--- case_2.java
+++ case_2.java
@@ -1,19 +1,62 @@
-abstract class Device{
+abstract class Device {
     protected String brand;
-    public Device (String brand){
+
+    public Device(String brand) {
         this.brand = brand;
     }
+
     public abstract void turnOn();
 }
 
-interface RemoteControllablae {
+interface RemoteControllable { 
     void connectToWifi();
 }
 
 interface PowerSaving {
-    String getEnergyRating ();
+    String getEnergyRating();
+}
+
+class SmartTV extends Device implements RemoteControllable, PowerSaving { 
+    public SmartTV(String brand) {
+        super(brand);
+    }
+
+    @Override
+    public void turnOn() {
+        System.out.println(brand + " Smart TV is now ON.");
+    }
+
+    @Override
+    public void connectToWifi() {
+        System.out.println(brand + " Smart TV connected to WiFi.");
+    }
+
+    @Override
+    public String getEnergyRating() {
+        return "A+";
+    }
+}
+
+class ElectricKettle extends Device {
+    public ElectricKettle(String brand) {
+        super(brand);
+    }
+
+    @Override
+    public void turnOn() {
+        System.out.println(brand + " Electric Kettle is now ON.");
+    }
 }
 
 public class case_2 {
-    
+    public static void main(String[] args) {
+        System.out.println("--- Test Case 1 ---");
+        Device d = new SmartTV("Sony");
+        d.turnOn(); 
+        System.out.println("\n--- Test Case 2 ---");
+        RemoteControllable r = new SmartTV("LG");
+        r.connectToWifi();
+        System.out.println("\n--- Test Case 3 ---");
+        
+    }
 }
